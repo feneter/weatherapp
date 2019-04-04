9cf3c0fe1a655855b809c4784528b7f689587dc1
yudathadei mdamu <thasjonior@hotmail.com>
2019-04-04 10:33:30 +0300

    new repository

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 const api = 'api.openweathermap.org/data/2.5/weather';
-const id = 160260;
-const appid = ''; //add your appid here, and remember to remove it before pushing to github
+let city = 'njombe';
+const country = 'tz';
+const appid = 'd3243a8091f7937403f8541ce92c32c8'; //add your appid here, and remember to remove it before pushing to github
 
 update();
 /**
@@ -17,18 +18,27 @@ setInterval(() => {
  * we can extend this to send anything other than modifying the DOM.
  * 
  */
-function update(){
+
+	function update(){
     console.log("Updating...");
     //fetch(`https://${api}?id=${id}&appid=${appid}`)
-    fetch("https://" + api + "?id=" + id + "&appid=" + appid)
+	//https://api.openweathermap.org/data/2.5/weather?q=Njombe,tz&appid=d3243a8091f7937403f8541ce92c32c8
+   try{ fetch("https://"+api+"?q="+city+","+country+"&appid="+ appid)
     .then(response => response.json())
     .then(data => {
+		console.log(data);
         printTemperature(data.main.temp, data.main.temp_max, data.main.temp_min);
         printWindData(data.wind.speed, data.wind.deg)
         printClouds(data.clouds.all);
         printPrecipitation(data.main.humidity);
-    });
+		printPlace(data.name);
+   });}
+   catch(err){
+	   alert("Enter the name of City in Tanzania");
+   }
 }
+	
+
 
 
 function printWindData(speed, direction) {
@@ -39,6 +49,11 @@ function printWindData(speed, direction) {
 
 function printClouds(cloud){
     setDomElement('cloud', cloud);
+}
+function printPlace(city)
+{
+document.getElementById('city').innerHTML=city+ " - weather now";
+	
 }
 
 function printPrecipitation(humidity) {
@@ -85,3 +100,68 @@ function kelvinToCelcius(kelvin) {
 function kelvinToFahrenheit(kelvin) {
     return `${kelvin-273}F`;
 }
+let search =document.getElementById('search_box');
+search.addEventListener('focus', function(){
+	console.log("user enters some value")
+});
+let btn= document.getElementById('btn');
+btn.addEventListener('click',function(){
+	let search_box= document.getElementById('search_box');
+	if(!search_box.value){
+		alert("You have to enter the city name");
+		return;
+	}
+	else{
+		city=search_box.value;
+		search_box.value='';
+		update();
+	}
+	
+});
+
+
+/*
+let search =document.getElementById('search');
+search.addEventListener('focus',function(){
+		console.log("user enters some value");
+});
+let search_button =document.getElementById('search_button');
+search_button.addEventListener('click', function(){
+	
+	let search_box= document.getElementById("location");
+	if(!search_box.value){
+		alert('You have to Enter city name');
+		return;
+	}
+	city=search_box.value;
+	search_box.value='';
+	update();	
+});
+ */
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
